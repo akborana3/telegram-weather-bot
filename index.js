d74ccb020f93883216baf4195f228e0322b27f5a
A K BORANA <88649979+akborana3@users.noreply.github.com>
2024-04-17 23:15:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 // replace the value below with the Telegram token you receive from @BotFather
-const token = "token";
+const token = "2136045481:AAFfO1sp7DJqt15mgjP7BoiUZSAcfip2cyo";
 
 const bot = new TelegramBot(token, { polling: true });
 
